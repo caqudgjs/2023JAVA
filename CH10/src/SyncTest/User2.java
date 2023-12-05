806dd38746eadf2189d4800579e61b201c77b95c
dullbear1125 <>
2023-12-05 09:52:26 +0900

    choi
    plus thread syncronized

---
+++ CH10/src/SyncTest/User2.java
@@ -0,0 +1,19 @@
+package SyncTest;
+
+public class User2 extends Thread{
+
+private Calculator cal;
+	
+	public void setCalculator(Calculator cal) {
+		this.cal = cal;
+		this.setName("User2");
+	}
+	
+	
+	public void run() {
+		cal.setMemory(50);
+		
+	}
+}
+
+
